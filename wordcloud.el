c0e65de9e6f41e4e373f47308116c83f63ab70e2
Dave Pearson <davep@davep.org>
2017-12-08 13:49:13 +0000

    Bump to v1.00

--- wordcloud.el
+++ wordcloud.el
@@ -2,7 +2,7 @@
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 0.01
+;; Version: 1.00
 ;; Keywords: games
 ;; URL: https://github.com/davep/wordcloud.el
 ;; Package-Requires: ((cl-lib "0.5") (emacs "24.3"))
